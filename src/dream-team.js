79381ba4e4df51ab10c3023fd939a733149aef3d
vlad23349 <78217932+vlad23349@users.noreply.github.com>
2021-03-29 14:43:08 +0300

    feat: update task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let str = '';
+  let alph = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0,
+    E: 0,
+    F: 0,
+    G: 0,
+    H: 0,
+    I: 0,
+    J: 0,
+    K: 0,
+    L: 0,
+    M: 0,
+    N: 0,
+    O: 0,
+    P: 0,
+    Q: 0,
+    R: 0,
+    S: 0,
+    T: 0,
+    U: 0,
+    V: 0,
+    W: 0,
+    X: 0,
+    Y: 0,
+    Z: 0,
+  }
+  for (let i = 0; i < members.length; i++) {
+    if (typeof (members[i]) == 'string')
+      members[i] = members[i].replace(/ /g, '');
+  }
+  for (let i = 0; i < members.length; i++) {
+    if (typeof (members[i]) == 'string')
+      alph[members[i][0].toUpperCase()]++;
+  }
+  for (let el in alph) {
+    if (alph[el] > 0) {
+      for (let i = 0; i < alph[el]; i++) {
+        str += el;
+
+      }
+    }
+  }
+  return (str);
 };
