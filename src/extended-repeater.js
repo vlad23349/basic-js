f44348547d33557b076c971b52835824c7bf5d13
vlad23349 <78217932+vlad23349@users.noreply.github.com>
2021-03-29 16:14:04 +0300

    feat: update task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  str = str + '';
+  if (options['additionSeparator'] === undefined) options['additionSeparator'] = '|';
+  for (let el in options) {
+    options[el] = options[el] + '';
+  }
+  if (options['addition']) {
+    if (options['additionRepeatTimes']) {
+      for (let i = 0; i < options['additionRepeatTimes']; i++) {
+        str += options['addition'];
+        if (!(i == options['additionRepeatTimes'] - 1))
+          str += options['additionSeparator'];
+      }
+    }
+    else {
+      str += options['addition'];
+    }
+  }
+  let temp = str;
+  if (options['repeatTimes']) {
+    if (options['separator']) {
+      for (let i = 0; i < options['repeatTimes'] - 1; i++) {
+        str += options['separator'];
+        str += temp;
+      }
+    }
+    else {
+      options['separator'] = '+';
+      for (let i = 0; i < options['repeatTimes'] - 1; i++) {
+        str += options['separator'];
+        str += temp;
+      }
+    }
+  }
+
+  return str;
 };
   
